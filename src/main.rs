3b34836cb5c074a8d99832d93d85c7baecaed278
Martin Celavie <martin.rabat@gmail.com>
2025-09-08 18:31:27 +0200

    netoyage des urls

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use scraper::{Html, Selector}; 
+use url::Url;
 ///un scraper web simple qui télécharge recursivement les images d'un site.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,8 +24,39 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("URL: {}", args.url);
-    println!("Recursive: {}", args.recursive);
-    println!("Level: {}", args.level);
-    println!("Path: {}", args.path);
+    
+    //On transforme l'URL de l'utilisateur en une URL de base structurée
+    let base_url = Url::parse(&args.url)
+        .expect("ERREUR : URL de base invalide.");
+    println!("Récupération de l'URL : {}", base_url);
+
+    let response = reqwest::blocking::get(&args.url)
+        .expect("ERREUR : Impossible de récupérer l'URL.");
+    
+    let body = response.text()
+        .expect("ERREUR : Impossible de lire le corps de la réponse.");
+
+    println!("--- Analyse du HTML ---");
+
+    // On transforme le texte HTML en un document qu'on peut interroger
+    let document = Html::parse_document(&body);
+
+    // On crée un "sélecteur" CSS pour trouver toutes les balises <img>
+    let image_selector = Selector::parse("img").unwrap();
+
+    // On cherche tous les éléments qui correspondent à notre sélecteur
+    for element in document.select(&image_selector) {
+        // Pour chaque élément trouvé, on essaie de récupérer son attribut "src"
+        if let Some(src) = element.value().attr("src") {
+            match base_url.join(src) {
+                Ok(full_url) => {
+                    println!("URL Absolue Trouvée : {}", full_url);
+                }
+                Err(e) => {
+                    // On affiche une erreur si l'URL est malformée, mais on continue
+                    println!("Impossible de traiter l'URL '{}': {}", src, e);
+                }
+            }
+        }
+    }
 }
